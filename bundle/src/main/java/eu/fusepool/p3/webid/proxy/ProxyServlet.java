9f3877792445be6b5ed3ccb13a3c371e2fd750f9
Pascal Mainini <pascal@mainini.ch>
2014-12-05 12:06:23 +0100

    initially added proxy-implementation taken from p3-proxy

--- bundle/src/main/java/eu/fusepool/p3/webid/proxy/ProxyServlet.java
+++ bundle/src/main/java/eu/fusepool/p3/webid/proxy/ProxyServlet.java
@@ -16,31 +16,167 @@
 package eu.fusepool.p3.webid.proxy;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.RedirectStrategy;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.log.LogService;
 
 /**
+ * A servlet which proxies HTTP-Requests to another HTTP-Server.
  *
  * @author Pascal Mainini
  */
-@SuppressWarnings("serial")
 // refer to https://felix.apache.org/documentation/subprojects/apache-felix-http-service.html
-@Component(service=Servlet.class, property={"alias=/hello"})
+@Component(service = Servlet.class, property = {"alias=/"})
+@SuppressWarnings("serial")
 public class ProxyServlet extends HttpServlet {
+    private static final String targetBaseUri = "http://ti.bfh.ch/";
+    private final CloseableHttpClient httpclient;
+    private LogService log;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
+    /**
+     * Initializes the servlet by setting up the HTTP-client.
+     */
+    public ProxyServlet() {
+        final HttpClientBuilder hcb = HttpClientBuilder.create();
+        hcb.setRedirectStrategy(new NeverRedirectStrategy());
+        httpclient = hcb.build();
     }
 
+    /**
+     * DS-binding for setting the log-service when it becomes available.
+     *
+     * @param service the log-service to set
+     */
+    @Reference(
+            name = "log.service",
+            service = LogService.class,
+            unbind = "unsetLogService"
+    )
+    protected void setLogService(LogService service) {
+        this.log = service;
+        log.log(LogService.LOG_INFO, "Obtained logservice!");
+    }
+
+    /**
+     * DS-binding for removing the log-service when it's no longer available.
+     *
+     * @param service the log-service being removed
+     */
+    protected void unsetLogService(LogService service) {
+        this.log = null;
+    }
+
+    /**
+     * The service method from HttpServlet, performs handling of all
+     * HTTP-requests independent of their method. Requests and responses within
+     * the method can be distinguished by belonging to the "frontend" (i.e. the
+     * client connecting to the proxy) or the "backend" (the server being
+     * contacted on behalf of the client)
+     *
+     * @param frontendRequest Request coming in from the client
+     * @param frontendResponse Response being returned to the client
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        PrintWriter pw = resp.getWriter();
-        pw.println("Hello, world!");    }    
+    protected void service(final HttpServletRequest frontendRequest, final HttpServletResponse frontendResponse)
+            throws ServletException, IOException {
+        final String targetUrlString = targetBaseUri + frontendRequest.getRequestURI();
+        final URL targetUrl = new URL(targetUrlString);
+
+        //////////////////// Setup backend request
+        final HttpEntityEnclosingRequestBase backendRequest = new HttpEntityEnclosingRequestBase() {
+            @Override
+            public String getMethod() {
+                return frontendRequest.getMethod();
+            }
+        };
+        try {
+            backendRequest.setURI(targetUrl.toURI());
+        } catch (URISyntaxException ex) {
+            throw new IOException(ex);
+        }
+
+        //////////////////// Copy headers to backend request
+        final Enumeration<String> headerNames = frontendRequest.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            final String headerName = headerNames.nextElement();
+            final Enumeration<String> headerValues = frontendRequest.getHeaders(headerName);
+            while (headerValues.hasMoreElements()) {
+                final String headerValue = headerValues.nextElement();
+                if (!headerName.equalsIgnoreCase("Content-Length")) {
+                    backendRequest.setHeader(headerName, headerValue);
+                }
+            }
+        }
+
+        //////////////////// Copy Entity - if any
+        final byte[] inEntityBytes = IOUtils.toByteArray(frontendRequest.getInputStream());
+        if (inEntityBytes.length > 0) {
+            backendRequest.setEntity(new ByteArrayEntity(inEntityBytes));
+        }
+
+        //////////////////// Execute request to backend
+        try (CloseableHttpResponse backendResponse = httpclient.execute(backendRequest)) {
+            frontendResponse.setStatus(backendResponse.getStatusLine().getStatusCode());
+
+            // Copy back headers
+            @SuppressWarnings("unchecked")
+            final Set<String> setHeaderNames = new HashSet();
+            for (Header header : backendResponse.getAllHeaders()) {
+                if (setHeaderNames.add(header.getName())) {
+                    frontendResponse.setHeader(header.getName(), header.getValue());
+                } else {
+                    frontendResponse.addHeader(header.getName(), header.getValue());
+                }
+            }
+
+            final ServletOutputStream outStream = frontendResponse.getOutputStream();
+
+            // Copy back entity
+            final HttpEntity entity = backendResponse.getEntity();
+            if (entity != null) {
+                try (InputStream inStream = entity.getContent()) {
+                    IOUtils.copy(inStream, outStream);
+                }
+            }
+            outStream.flush();
+        }
+    }
+
+    private static class NeverRedirectStrategy implements RedirectStrategy {
+        @Override
+        public HttpUriRequest getRedirect(HttpRequest hr, HttpResponse hr1, org.apache.http.protocol.HttpContext hc) throws org.apache.http.ProtocolException {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public boolean isRedirected(HttpRequest hr, HttpResponse hr1, org.apache.http.protocol.HttpContext hc) throws org.apache.http.ProtocolException {
+            return false;
+        }
+    }
 }
